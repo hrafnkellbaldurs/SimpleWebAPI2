44d8321210a55c57d11f83355994e1387ccbd333
kotkarl <kotkarl@gmail.com>
2015-09-05 15:45:58 +0000

    first commit

---
+++ SimpleWebAPI2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SimpleWebAPI2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ SimpleWebAPI2/Models/CourseRegistration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleWebAPI2.Models
+{
+    public class CourseRegistration
+    {
+    }
+}
