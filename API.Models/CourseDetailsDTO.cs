8576d20ee44cb13437ce29a2ada87500ab889293
Rannveigg13 <rannveigg13@ru.is>
2015-09-05 17:34:50 +0000

    API.Models added

---
+++ API.Models/CourseDetailsDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    /// <summary>
+    /// THis class represents a single course, and contains various
+    /// details about the course
+    /// </summary>
+    public class CourseDetailsDTO
+    {
+        /// <summary>
+        /// The ID of the course
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// The name of the course
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// List of students in the course
+        /// </summary>
+        public List<StudentDTO>  Students { get; set; }
+
+        /// <summary>
+        /// Description of the course
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
