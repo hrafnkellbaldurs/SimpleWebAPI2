5dcfb3aac1a7903b88768bcbb57eaf6e69c6c059
kotkarl <kotkarl@gmail.com>
2015-09-05 18:10:02 +0000

    Added some models

--- SimpleWebAPI2/Models/CourseRegistration.cs
+++ SimpleWebAPI2/Models/CourseRegistration.cs
@@ -5,7 +5,27 @@ using System.Web;
 
 namespace SimpleWebAPI2.Models
 {
+    /// <summary>
+    /// This class represents a link between a course and a student
+    /// </summary>
     public class CourseRegistration
     {
+        /// <summary>
+        /// The ID of a single course registration.
+        /// Example: 42
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// The ID of a course linked with a student.
+        /// Example: 1
+        /// </summary>
+        public int CourseID { get; set; }
+
+        /// <summary>
+        /// The ID of a student linked with a course.
+        /// Example: 30
+        /// </summary>
+        public int StudentID { get; set; }
     }
 }
--- SimpleWebAPI2/Models/Student.cs
+++ SimpleWebAPI2/Models/Student.cs
@@ -5,7 +5,28 @@ using System.Web;
 
 namespace SimpleWebAPI2.Models
 {
+    /// <summary>
+    /// This class represents a single student at a school
+    /// </summary>
     public class Student
     {
+        /// <summary>
+        /// The ID of a student.
+        /// Example: 32
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        ///  The name of a student.
+        ///  Example: "Gunnar Atlason"
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// The Social Security Number of a student. 
+        /// Used as a unique identifier.
+        /// Example: "3012933419"
+        /// </summary>
+        public String SSN { get; set; }
     }
 }
